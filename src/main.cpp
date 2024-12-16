0af69a6e7c31f6561b73231c4dfbc00f79530a14
R Huish <5207490+genestealer@users.noreply.github.com>
2024-12-16 16:09:37 +0000

    Updated header

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,58 @@
 /***************************************************
-  Irrigation Controller
-  Richard Huish 2017-2024
-  Controllino Maxi Ethernet-based with local home-assistant.io GUI,
-    relay output for dual irrigation control via MQTT
-    MQTT command message with 'on' payload commands one of the outputs on.
-    MQTT command message with 'off' payload commands one of the outputs off.
-  Note: My code is based on my other ESP8266-based projects, so there may be some ESP naming used.
-  ESP8266 wifi variant: https://github.com/genestealer/Irrigation-Controller
-  ----------
-  Github: https://github.com/genestealer/Controllino-Irrigation
-  ----------
-  GUI: Locally hosted home assistant on network https://www.home-assistant.io/
-  MQTT: Locally hosted broker on network https://mosquitto.org/
-  OTA updates: Not supported by default by the ATmega without flashing with a custom bootloader and I have not done this.
-  ----------
-  The circuit:
-    Controllino Maxi - ATmega 2560-16AU with W5100 ethernet https://www.controllino.biz/product/controllino-maxi/
-      pinout: https://www.controllino.biz/wp-content/uploads/2018/10/CONTROLLINO-MAXI-Pinout.pdf
-    CONTROLLINO customized bootloaders https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library/tree/master/Bootloaders/MAXI
-    W5100 ethernet (Built-In)
-    12V PoE Active splitter Adapter, to power Controllino
-    12V Electric Solenoid Valve for Water
-    Flyback diode: Vishay 50V 1A, Diode, 2-Pin DO-204AL 1N4001-E3/54
-  Inputs:
-    Analog Capacitive Soil Moisture Sensor V1.2 https://www.aliexpress.com/item/32832538686
-  Outputs:
-    CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_10 (2 Amp output) - 1st water valve
-    CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_11 (2 Amp output) - 2nd water valve
-        (Note: You could use the relays to switch higher voltages or have galvanic isolation)
-    Multiple on-board LEDs to show MQTT connection, ethernet connection, status, etc.
-  ----------
-  Example Bill Of Materials:
-    Controllino Maxi https://www.controllino.biz/product/controllino-maxi/
-    1/2 Inch 12V Electric Solenoid Valve for Water https://www.aliexpress.com/item/32951916193.html
-    Active 12V PoE power over ethernet Splitter Adapter, IEEE 802.3af Compliant 10/100Mbps, 12V output https://www.aliexpress.com/item/32620368747.html
-  ----------
-  Edits made to the PlatformIO Project Configuration File:
-    build_flags = -DMQTT_MAX_PACKET_SIZE=512 // Override max JSON size, until library is updated to include this option https://github.com/knolleary/pubsubclient/issues/110#issuecomment-174953049
+  Irrigation Controller - Controllino Maxi (Ethernet-based)
+  Author: Richard Huish (2017-2024)
+  
+  **Description:**
+  Dual irrigation system with local control via Home Assistant GUI and MQTT integration.
+  - MQTT 'on' payload commands one output on.
+  - MQTT 'off' payload commands one output off.
+  - Ethernet connectivity via W5100.
+  - Based on previous ESP8266-based projects: https://github.com/genestealer/Irrigation-Controller
+
+  **Project Repository:**
+  GitHub: https://github.com/genestealer/Controllino-Irrigation
+
+  **System Overview:**
+  - GUI: Locally hosted Home Assistant (https://www.home-assistant.io/)
+  - MQTT Broker: Mosquitto (https://mosquitto.org/)
+  - OTA Updates: Not supported on ATmega without custom bootloader flashing.
+
+  **Hardware Components:**
+  - Controller: Controllino Maxi (ATmega 2560-16AU with W5100 Ethernet)
+    - Datasheet: https://www.controllino.biz/product/controllino-maxi/
+    - Pinout: https://www.controllino.biz/wp-content/uploads/2018/10/CONTROLLINO-MAXI-Pinout.pdf
+    - Bootloaders: https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library/tree/master/Bootloaders/MAXI
+  - Power: 12V PoE Active Splitter Adapter (IEEE 802.3af compliant)
+  - Valves: 12V Electric Solenoid Valves for water control
+  - Protection: Vishay 1N4001 Flyback Diodes (50V 1A)
+  - Sensor: Analog Capacitive Soil Moisture Sensor V1.2 (https://www.aliexpress.com/item/32832538686)
+
+  **Connections:**
+  - Outputs:
+    - CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_10: 1st Water Valve (2A output)
+    - CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_11: 2nd Water Valve (2A output)
+    - CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_09: 3rd Water Valve (2A output)
+    - CONTROLLINO_SCREW_TERMINAL_DIGITAL_OUT_08: 4th Water Valve (2A output)
+      *Note: Relays can switch higher voltages or provide galvanic isolation.
+  - Inputs:
+    - Analog Soil Moisture Sensor.
+  - Indicators:
+    - On-board LEDs for MQTT, Ethernet, and system status.
+
+  **Bill of Materials (BOM):**
+  - Controllino Maxi: https://www.controllino.biz/product/controllino-maxi/
+  - 12V Solenoid Valve: https://www.aliexpress.com/item/32951916193.html
+  - 12V PoE Splitter: https://www.aliexpress.com/item/32620368747.html
+
+  **PlatformIO Configuration:**
+    platform = atmelavr
+    board = controllino_maxi
+    framework = arduino
+    lib_deps = PubSubClient, ArduinoJson, SPI, Ethernet, Controllino, I2CSoilMoistureSensor, Wire
+    monitor_speed = 115200
+
 ****************************************************/
+
 
 // Note: Libraries are included in "Project Dependencies" file platformio.ini
 #include <private.h>      // Passwords etc. not for GitHub
